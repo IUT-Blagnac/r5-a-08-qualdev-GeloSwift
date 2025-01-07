05ec372d498467d867001aee8b86b42d77390fb7
GeloSwift <angelo.nichele317@gmail.com>
2025-01-07 16:59:36 +0100

    TP1

--- hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -1,10 +1,15 @@
 package hellocucumber;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class StepDefinitions {
+
+    private String today;
+    private String actualAnswer;
 
     @Given("an example scenario")
     public void anExampleScenario() {
@@ -18,22 +23,25 @@ public class StepDefinitions {
     public void theScenarioPasses() {
     }
 
-    @Given("today is Sunday")
-    public void today_is_sunday() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @When("I ask whether it's Friday yet")
-    public void i_ask_whether_it_s_friday_yet() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("I should be told {string}")
-    public void i_should_be_told(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Given("today is {string}")
+    public void today_is(String d) {
+        this.today = d;
     }
 
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_friday_yet() {
+        this.actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
 
 }
 
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
